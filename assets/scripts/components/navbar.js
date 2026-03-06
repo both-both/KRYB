22d42c1368a372cd7c1150ec23d8551849b6f733
both-both <claraboth@gmail.com>
2026-03-06 10:23:40 +0100

    ny img

--- assets/scripts/components/navbar.js
+++ assets/scripts/components/navbar.js
@@ -1,7 +1,7 @@
 import { create } from '../utils/create.js';
 import { get } from "../utils/get.js";
 import { set } from "../utils/set.js";
-import shopImg from '../../images/svg/Icon-Search.svg'
+import shopImg from '../../images/svg/Icon-Basket.svg'
 
 export function navbar() {
     const navbar = create('div')
